3fd4fcb429113f72384ac8280a852f28e242b121
egai <egaiosowor@yahoo.com>
2024-09-28 06:08:36 -0700

    fetch user name in user card

--- src/components/topbar/userCard.jsx
+++ src/components/topbar/userCard.jsx
@@ -1,27 +1,20 @@
 import Link from "next/link";
 
-import { CgProfile } from "react-icons/cg";
-import { VscSettings } from "react-icons/vsc";
+import { fetchProfile } from "@/utils/data";
+import { createClient } from '@/utils/supabase/server'
 
+const UserMenu = async () => {
+    const supabase = createClient()
 
-const MenuItems = [
-    {
-        title: "Profile",
-        icon: <CgProfile />,
-        path: "/dashboard/profile",
-    },
-    {
-        title: "Settings",
-        icon: <VscSettings />,
-        path: "/dashboard/settings",
-    },
-]
+    const {
+        data: { user },
+    } = await supabase.auth.getUser()
+    const profile = await fetchProfile(user)
 
-const UserMenu = () => {
-    return(
+    return (
         <Link href={'/dashboard/profile'} className="px-4 flex items-center space-x-2 text-gray-900 cursor-pointer">
-            <img className="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-4.jpg" alt="Jese image"/>
-            <p className="text-base font-semibold">Neil Sims</p>
+            <img className="w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-4.jpg" alt="Jese image" />
+            <p className="text-base font-semibold">{`${profile?.first_name} ${profile?.last_name}`}</p>
         </Link>
     )
 }
