eff18cb018e615b8accb369f9506fd0dfd177d18
egai <egaiosowor@yahoo.com>
2024-10-04 11:20:15 -0700

    restyle bioform loading skeleton

--- src/components/skeletons/bioFormSkeleton.jsx
+++ src/components/skeletons/bioFormSkeleton.jsx
@@ -2,9 +2,9 @@ export default function BioFormSkeleton(){
     return(
         <div className="animate-pulse flex items-center bg-white rounded-xl p-6 space-x-2" >
             <div className="w-16 h-16 bg-gray-200  rounded-full" ></div>
-            <div className="">
-                <div className="h-3 w-40 bg-gray-200" ></div>
-                <div className="h-2 w-16 bg-gray-200" ></div>
+            <div className="space-y-2">
+                <div className="h-3 w-40 bg-gray-200 rounded-full" ></div>
+                <div className="h-3 w-16 bg-gray-200 rounded-full" ></div>
             </div>
         </div>
     )
