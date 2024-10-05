cda5c93def0b099e2358bb03ead31332f00ca57e
egai <egaiosowor@yahoo.com>
2024-10-05 11:36:15 -0700

    abstract menu items from navbar

---
+++ src/components/layout/navbar/menuItem.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+
+export default function MenuItem({item, pathname}){
+    return(
+        <li key={item.title} className="space-y-3" >
+            <h4 className="ml-7 text-xs text-[#556476] font-semibold" >{item.title}</h4>
+            <ul className="space-y-1" >
+                {item.list.map((obj) => (
+                    <li key={obj.path} >
+                        <Link href={obj.path} className={`flex space-x-2 items-center text-[#556476] py-3 pl-7 pr-14 ${pathname === obj.path && "bg-[#F4F7FF] !text-[#4A58EC]"} hover:text-[#4A58EC] hover:bg-[#F4F7FF]`}>
+                            {obj.icon}
+                            <p>{obj.title}</p>
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </li>
+    )
+}
--- src/components/layout/navbar/navbar.jsx
+++ src/components/layout/navbar/navbar.jsx
@@ -9,7 +9,8 @@ import { FaRegUser } from "react-icons/fa";
 import { IoPersonAddOutline } from "react-icons/io5";
 import { RiGroupLine } from "react-icons/ri";
 
-import SignOutButton from "../ui/signOutButton";
+import MenuItem from "./menuItem";
+import SignOutButton from "../../ui/signOutButton";
 
 
 const MenuItems = [
@@ -58,21 +59,9 @@ const Navbar = () => {
        <nav className="py-12 h-screen flex flex-col justify-between border-r" >
         <Link href={'/dashboard'} className="ml-7 font-bold text-green-700 text-xl">Logo</Link>
         <ul className="space-y-12" >
-            {MenuItems.map((item) => (
-                    <li key={item.title} className="space-y-3" >
-                        <h4 className="ml-7 text-xs text-[#556476] font-semibold" >{item.title}</h4>
-                        <ul className="space-y-1" >
-                            {item.list.map((listItem) => (
-                                <li key={listItem.path} >
-                                    <Link href={listItem.path} className={`flex space-x-2 items-center text-[#556476] py-3 pl-7 pr-14 ${pathname === listItem.path && "bg-[#F4F7FF] !text-[#4A58EC]"} hover:text-[#4A58EC] hover:bg-[#F4F7FF]`}>
-                                        {listItem.icon}
-                                        <p>{listItem.title}</p>
-                                    </Link>
-                                </li>
-                            ))}
-                        </ul>
-                    </li>
-            ))}
+            {
+                MenuItems.map(item => <MenuItem item={item} pathname={pathname}/>
+            )}
         </ul>
         <SignOutButton/>
         </nav>
