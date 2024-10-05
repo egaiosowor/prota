bb29cea7daaebfd587b4c4927ab3e60f24000b57
egai <egaiosowor@yahoo.com>
2024-10-05 11:20:08 -0700

    fix header import in layout

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/layout/navbar";
-import Header from "@/components/header/header";
+import Header from "@/components/layout/header";
 
 
 const Layout = ({children}) => {
