aaf2bf49b9f5dbf789d2ad2702a913cd3dc9f6ec
egaiosowor <egaiosowor@yahoo.com>
2025-01-11 15:06:19 +0100

    update site logo and favicon

--- src/components/layout/navbar/navbar.tsx
+++ src/components/layout/navbar/navbar.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { signOut } from "@/actions/actions";
 
-import { LuLogOut } from "react-icons/lu";  
+import { LuLogOut } from "react-icons/lu";
 import { LuLayoutDashboard } from "react-icons/lu";
 import { HiOutlineOfficeBuilding } from "react-icons/hi";
 import { FaRegUser } from "react-icons/fa";
@@ -13,78 +13,72 @@ import { IoPersonAddOutline } from "react-icons/io5";
 import { RiGroupLine } from "react-icons/ri";
 
 import MenuList from "./menuList";
+import { Logo } from "@/components/ui/logo";
 
-
-const menu_list= [
-    {
-        title: "MENU",
-        list: [
-            {
-                title: "Dashboard",
-                icon: <LuLayoutDashboard />,
-                path: "/dashboard",
-            },
-            {
-                title: "Accounts",
-                icon: <HiOutlineOfficeBuilding />,
-                path: "/dashboard/accounts",
-            },
-            {
-                title: "Users",
-                icon: <FaRegUser />,
-                path: "/users",
-            }
-        ]
-    },
-    {
-        title: "LEADS",
-        list: [
-            {
-                title: "Contacts",
-                icon: <IoPersonAddOutline />,
-                path: "/dashboard/contacts",
-            },
-            {
-                title: "Leads",
-                icon: <RiGroupLine />,
-                path: "/dashboard/leads",
-            }
-        ]
-    }
-]
+const menu_list = [
+  {
+    title: "MENU",
+    list: [
+      {
+        title: "Dashboard",
+        icon: <LuLayoutDashboard />,
+        path: "/dashboard",
+      },
+      {
+        title: "Accounts",
+        icon: <HiOutlineOfficeBuilding />,
+        path: "/dashboard/accounts",
+      },
+      {
+        title: "Users",
+        icon: <FaRegUser />,
+        path: "/users",
+      },
+    ],
+  },
+  {
+    title: "LEADS",
+    list: [
+      {
+        title: "Contacts",
+        icon: <IoPersonAddOutline />,
+        path: "/dashboard/contacts",
+      },
+      {
+        title: "Leads",
+        icon: <RiGroupLine />,
+        path: "/dashboard/leads",
+      },
+    ],
+  },
+];
 
 const Navbar = () => {
+  const pathname = usePathname();
 
-    const pathname = usePathname()
-
-    return(
-       <nav className="py-12 h-screen flex flex-col justify-between border-r" >
-        <Link href={'/dashboard'} className="ml-7 font-bold text-green-700 text-xl">Logo</Link>
-        <ul className="space-y-12" >
-            {
-                menu_list.map(item => (
-                    <MenuList key={item.title} menu_item={item} pathname={pathname}/>
-                ))}
-            </ul>
-        <SignOutButton/>
-        </nav>
-    ) 
-}
+  return (
+    <nav className="py-12 h-screen flex flex-col justify-between border-r">
+      <Logo />
+      <ul className="space-y-12">
+        {menu_list.map((item) => (
+          <MenuList key={item.title} menu_item={item} pathname={pathname} />
+        ))}
+      </ul>
+      <SignOutButton />
+    </nav>
+  );
+};
 
 export default Navbar;
 
-  
-
-
-function SignOutButton(){
-    return(
-        <button onClick={()=>signOut()} className={`flex space-x-2 items-center text-[#556476] py-3 pl-7 pr-14 hover:text-[#4A58EC] hover:bg-[#F4F7FF]`}>
-            <LuLogOut />
-            <p>Logout</p>
-        </button>
-    )
+function SignOutButton() {
+  return (
+    <button
+      onClick={() => signOut()}
+      className={`flex space-x-2 items-center text-[#556476] py-3 pl-7 pr-14 hover:text-[#4A58EC] hover:bg-[#F4F7FF]`}
+    >
+      <LuLogOut />
+      <p>Logout</p>
+    </button>
+  );
 }
-
-
-
-
